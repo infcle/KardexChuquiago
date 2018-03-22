3c4cfce7fd2935f54762cab48889670386fdb905
Laiwett <l.mendoza.liet@gmail.com>
2018-03-22 16:58:59 -0400

    menu

--- app/src/main/java/com/chuquiagomarca/kardexchuquiago/LoginActivity.java
+++ app/src/main/java/com/chuquiagomarca/kardexchuquiago/LoginActivity.java
@@ -1,9 +1,12 @@
 package com.chuquiagomarca.kardexchuquiago;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -13,6 +16,13 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     private EditText _etUsuario, _etContrasenia;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // -->Poner la pantalla en vertical
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        // -->Poner Pantalla Completa
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         _etUsuario     = findViewById(R.id.et_user);
@@ -25,7 +35,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     @Override
     public void onClick(View view) {
-        Intent i = new Intent(this,InicioActivity.class);
-        startActivity(i);
+        Intent m = new Intent(this,MenuActivity.class);
+        startActivity(m);
     }
 }
---
+++ app/src/main/java/com/chuquiagomarca/kardexchuquiago/MenuActivity.java
@@ -0,0 +1,32 @@
+package com.chuquiagomarca.kardexchuquiago;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MenuActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button _btnKardexCursos, _btnConfiguraciones;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+        _btnKardexCursos    = findViewById(R.id.btn_KardexCursos);
+        _btnConfiguraciones = findViewById(R.id.btn_Configuraciones);
+        _btnKardexCursos.setOnClickListener(this);
+        _btnConfiguraciones.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.btn_KardexCursos){
+
+        }
+
+        if(view.getId() == R.id.btn_Configuraciones){
+
+        }
+    }
+}
